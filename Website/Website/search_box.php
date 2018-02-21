c675ff080b7cc23222ddffae6fec7c2936f85ce6
akshay-par <35957633+akshay-par@users.noreply.github.com>
2018-02-21 18:23:30 +0530

    Add files via upload

---
+++ Website/Website/search_box.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Getcryptopayments</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="icon" href="salogo.png" type="image/png" sizes="500x500">
+</head>
+<body>
+
+<!-- header START S.A.-->
+<?php include("header.php"); ?>
+<!-- header end S.A.-->
+
+<!-- 1 section -->
+<section class="home_bgslider">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h1 class="text-center">Store Directory</h1>
+         <h3 class="text-center">Largest directory of merchants accepting multiple cryptocurrencies</h3>
+      </div>
+    </div>
+  </div>
+  
+</section>
+<!-- 1 section/ -->
+
+<!-- 2 section -->
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center" style="margin-top: -25px;">
+         <form action="#" class="center_form">
+           <div class="myflexgroup">
+              <input type="text" placeholder="Search.." class="basis1">
+              <button type="submit" class="btn-search basis2">Search</button>
+           </div>
+          </form>
+
+      </div>
+     
+     
+     
+    </div>
+  </div>
+</section>
+
+<!-- 2 section/ -->
+
+
+<!-- 3 section -->
+
+<section>
+<br/><br/><br/><br/>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+      </div>
+
+      <div class="row">
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+      </div>
+      <div class="row">
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+      </div>
+      <div class="row">
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+       <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <div class="bg_box hvr-glow">
+          <a href="">
+            <img src="img/logo.png" class="img-responsive center-block">
+            <h3 class="text-center">Art & Collectibles</h3>
+          </a>
+        </div>
+      </div>
+
+      </div>
+
+    </div>
+   <br/><br/><br/>
+</section>
+
+<!-- 3 section/ -->
+
+
+
+<?php include("footer.php");?>
+
+
+</body>
+
+</html>

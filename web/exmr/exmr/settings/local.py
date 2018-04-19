6c7c884595f60028e642bd332eae88fcd15d539c
nija <ibin.elzy@gmail.com>
2018-04-19 18:11:27 +0530

    Two factor authentication

--- web/exmr/exmr/settings/local.py
+++ web/exmr/exmr/settings/local.py
@@ -10,10 +10,9 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'e12.ehosts.com'
-EMAIL_HOST_USER = 'noreply@getcryptopayments.net'
-EMAIL_HOST_PASSWORD = 'Gcp1234!!'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'exmr.test@gmail.com'
+EMAIL_HOST_PASSWORD = 'Adminqwerty123'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 DEBUG = True

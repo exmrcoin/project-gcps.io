1832697a7dc2c4f9b096be63100b97620adbd6e1
Nija P <nijap@techversantinfotech.com>
2018-04-30 14:30:42 +0530

    Email updated

--- web/exmr/exmr/settings/production.py
+++ web/exmr/exmr/settings/production.py
@@ -16,18 +16,27 @@ DATABASES = {
 
 
 # For email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#
+# EMAIL_USE_TLS = True
+#
+# EMAIL_HOST = 'e12.ehosts.com'
+#
+# EMAIL_HOST_USER = 'noreply@getcryptopayments.net'
+#
+# EMAIL_HOST_PASSWORD = 'Gcp1234!!'
+#
+# EMAIL_PORT = 587
+#
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-EMAIL_USE_TLS = True
 
-EMAIL_HOST = 'e12.ehosts.com'
-
-EMAIL_HOST_USER = 'noreply@getcryptopayments.net'
-
-EMAIL_HOST_PASSWORD = 'Gcp1234!!'
-
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'exmr.test@gmail.com'
+EMAIL_HOST_PASSWORD = 'Adminqwerty123'
 EMAIL_PORT = 587
-
+EMAIL_USE_TLS = True
+DEBUG = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 ADMINS = (

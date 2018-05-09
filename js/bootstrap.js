0f3d8f5a213baedf528ab6be5921358958b1162d
EXMR MONERO <32690091+eXMRcoin@users.noreply.github.com>
2018-05-08 23:13:07 -0400

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -752,7 +752,7 @@ if (typeof jQuery === 'undefined') {
  * Bootstrap: dropdown.js v3.3.7
  * http://getbootstrap.com/javascript/#dropdowns
  * ========================================================================
- * Copyright 2011-2016 Twitter, Inc.
+ * Copyright 2011-2016 Twitter , Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 

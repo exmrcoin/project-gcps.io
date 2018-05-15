e94bf5bd19484857f91c55dd5fdaba4a2a3750f4
pnija <nijap@techversantinfotech.com>
2018-05-15 21:31:45 +0530

    UIv2

--- web/exmr/staticfiles/js/common.js
+++ web/exmr/staticfiles/js/common.js
@@ -17,6 +17,7 @@ function formSubmit(formId, url) {
         success: function (data) {
             if (data.msg) {
                 // $.toaster(data.msg, 'Success', 'success');
+
                 $.notify({
                     icon: 'fa fa-warning-sign',
                     title: 'Success',
@@ -27,15 +28,33 @@ function formSubmit(formId, url) {
                             exit: 'animated fadeOutRight'
                         },
 
-                        type: 'SUCCESS'
+                        type: 'success'
 
                     });
                 result = true;
             }
             else {
+                var count = 500;
                 $.each(data, function (key, value) {
                     var $input = $(formId).find(":input[name='" + key + "']");
+                    $input.addClass('error-focus');
                     $input.siblings(".error").html(value[0]).show();
+                    window.setTimeout(function () {
+                    $.notify({
+                        icon: 'fa fa-warning-sign',
+                        title: 'warning',
+                        message: value[0]
+                    }, {
+                            animate: {
+                                enter: 'animated fadeInDown',
+                                exit: 'animated fadeOutDown'
+                            },
+    
+                            type: 'danger'
+    
+                        });
+                    }, count);
+                    count += 500;
                 });
                 result = false;
             }
